607a4ec1a61490a56a94e15a437e141ee1c88fd8
Ruslan Demchenko <ruslan.demchenko@gmail.com>
2022-12-07 09:35:59 -0500

    set some migrations to skip in testing env

--- database/migrations/2022_11_15_043658_create_wormhole_systems_table.php
+++ database/migrations/2022_11_15_043658_create_wormhole_systems_table.php
@@ -14,6 +14,10 @@ class CreateWormholeSystemsTable extends Migration
 
     public function up()
     {
+        if (app()->environment('testing')) {
+            return;
+        }
+        
         Schema::dropIfExists($this->tableName);
         Schema::create($this->tableName, function (Blueprint $table) {
             $table->id();
