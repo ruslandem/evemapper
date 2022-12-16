17688c674697d0705edd4c37cf66392509e5c5d0
Ruslan Demchenko <ruslan.demchenko@gmail.com>
2022-12-16 09:38:57 -0500

    Removed builded asets from git

--- public/build/assets/Index.ba5414e2.js
+++
@@ -1,1 +0,0 @@
-import{_ as e,c as o,o as t,a as c}from"./app.faa103b7.js";const n={},s=c("h2",null,"Route.vue",-1),_=[s];function a(r,d){return t(),o("div",null,_)}const u=e(n,[["render",a]]);export{u as default};
