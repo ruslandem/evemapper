eea5b6cb3b9505c73322ca135a68edbf025a04f1
Ruslan Demchenko <ruslan.demchenko@gmail.com>
2022-12-16 09:15:56 -0500

    Added public/build to gitignore

--- public/build/assets/Index.2f6d92cb.js
+++
@@ -1,1 +0,0 @@
-import{_ as e,c,o,a as t}from"./app.bce82e16.js";const n={},s=t("h2",null,"Locate.vue",-1),a=[s];function _(r,d){return o(),c("div",null,a)}const f=e(n,[["render",_]]);export{f as default};
