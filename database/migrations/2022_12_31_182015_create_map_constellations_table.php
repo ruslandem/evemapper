ffc34d2c24afc841614883c86c150bbef141ac6c
Ruslan Demchenko <ruslan.demchenko@gmail.com>
2022-12-31 13:44:09 -0500

    add migration for table mapConstellations

---
+++ database/migrations/2022_12_31_182015_create_map_constellations_table.php
@@ -0,0 +1,48 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Schema;
+
+class CreateMapConstellationsTable extends Migration
+{
+    protected $connection = 'app';
+
+    protected $tableName = 'mapConstellations';
+
+
+    public function up()
+    {
+        if (app()->environment('testing')) {
+            return;
+        }
+        
+        Schema::dropIfExists($this->tableName);
+        Schema::create($this->tableName, function (Blueprint $table) {
+            $table->integer('regionID');
+            $table->integer('constellationID')->primary();
+            $table->string('constellationName', 100);
+            $table->float('x');
+            $table->float('y');
+            $table->float('z');
+            $table->float('xMin');
+            $table->float('xMax');
+            $table->float('yMin');
+            $table->float('yMax');
+            $table->float('zMin');
+            $table->float('zMax');
+            $table->integer('factionID')->nullable();
+            $table->float('radius')->nullable();
+        });
+
+        DB::connection($this->connection)->unprepared(
+            file_get_contents('./database/init/mapConstellations.sql')
+        );
+    }
+
+    public function down()
+    {
+        Schema::dropIfExists($this->tableName);
+    }
+}
