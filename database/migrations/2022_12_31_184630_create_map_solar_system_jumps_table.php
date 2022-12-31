a78d36ec025d93f1d376d7d606543fbaefa3a805
Ruslan Demchenko <ruslan.demchenko@gmail.com>
2022-12-31 13:51:22 -0500

    add migration for table mapSolarSystemJumps

---
+++ database/migrations/2022_12_31_184630_create_map_solar_system_jumps_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Schema;
+
+class CreateMapSolarSystemJumpsTable extends Migration
+{
+    protected $connection = 'app';
+
+    protected $tableName = 'mapSolarSystemJumps';
+
+
+    public function up()
+    {
+        if (app()->environment('testing')) {
+            return;
+        }
+
+        Schema::dropIfExists($this->tableName);
+        Schema::create($this->tableName, function (Blueprint $table) {
+            $table->integer('fromRegionID')->nullable();
+            $table->integer('fromConstellationID')->nullable();
+            $table->integer('fromSolarSystemID');
+            $table->integer('toSolarSystemID');
+            $table->integer('toConstellationID')->nullable();
+            $table->integer('toRegionID')->nullable();
+            $table->primary(['fromSolarSystemID', 'toSolarSystemID']);
+        });
+
+        DB::connection($this->connection)->unprepared(
+            file_get_contents('./database/init/mapSolarSystemJumps.sql')
+        );
+    }
+
+    public function down()
+    {
+        Schema::dropIfExists($this->tableName);
+    }
+}
