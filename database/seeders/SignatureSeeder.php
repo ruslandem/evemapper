9a4fcad18b66a85876300295899ccaba0db8fb33
Ruslan Demchenko <ruslan.demchenko@gmail.com>
2022-12-31 15:45:46 -0500

    clear seeders

--- database/seeders/SignatureSeeder.php
+++ database/seeders/SignatureSeeder.php
@@ -2,18 +2,12 @@
 
 namespace Database\Seeders;
 
-use App\Models\Signature;
 use Illuminate\Database\Seeder;
-use Illuminate\Support\Facades\DB;
 
 class SignatureSeeder extends Seeder
 {
     public function run()
     {
-        DB::table('signatures')->truncate();
-
-        Signature::factory()
-            ->count(50)
-            ->create();
+        //
     }
 }
--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -2,17 +2,12 @@
 
 namespace Database\Seeders;
 
-use App\Models\User;
 use Illuminate\Database\Seeder;
 
 class UserSeeder extends Seeder
 {
     public function run()
     {
-        // DB::table('users')->truncate();
-
-        User::factory()
-            ->count(5)
-            ->create();
+        //
     }
 }
