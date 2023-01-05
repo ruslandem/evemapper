1200bbbb1e1f316e835ad19517a7339cb2f8af44
Ruslan Demchenko <ruslan.demchenko@gmail.com>
2023-01-04 21:03:56 -0500

    make new Eve api classes using abstract base

---
+++ app/Core/EveApi/AutopilotWaypointApiRequest.php
@@ -0,0 +1,58 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Core\EveApi;
+
+use App\Core\EveSolarSystem;
+use App\Core\Exceptions\EveApiException;
+use App\Core\Exceptions\EveApiTokenExpiredException;
+use App\Models\User;
+use Swagger\Client\Eve\ApiException;
+
+/**
+ * Add waypoint to character's autopilot.
+ */
+class AutopilotWaypointApiRequest extends AbstractApiRequest
+{
+    /**
+     * @param User $user
+     * @param array|null $data
+     * @throws EveApiTokenExpiredException
+     * @throws EveApiException
+     * @return array
+     */
+    public static function get(User $user, ?array $data): array
+    {
+        if (!$data['solarSystemName']) {
+            throw new EveApiException('solarSystemName is not set');
+        }
+
+        $api = self::getApiInstance('userInterfaceApi', $user);
+
+        try {
+            $api->postUiAutopilotWaypoint(
+                false,
+                false,
+                EveSolarSystem::getByName($data['solarSystemName']),
+                static::$dataSource,
+                $user->token
+            );
+        } catch (ApiException $e) {
+            $errorResponse = json_decode(
+                $e->getResponseBody(),
+                true,
+                512,
+                JSON_THROW_ON_ERROR
+            );
+
+            if ($errorResponse['error'] == 'token is expired') {
+                throw new EveApiTokenExpiredException('token is expired');
+            }
+
+            throw new EveApiException($e->getMessage(), $e->getCode());
+        }
+
+        return [];
+    }
+}
---
+++ app/Core/EveApi/LocationApiRequest.php
@@ -0,0 +1,56 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Core\EveApi;
+
+use App\Core\Exceptions\EveApiException;
+use App\Core\Exceptions\EveApiTokenExpiredException;
+use App\Models\User;
+use Swagger\Client\Eve\ApiException;
+use Swagger\Client\Eve\Model\GetCharactersCharacterIdLocationOk;
+
+/**
+ * Get current location of the character.
+ */
+class LocationApiRequest extends AbstractApiRequest
+{
+    /**
+     * @param User $user
+     * @throws EveApiTokenExpiredException
+     * @throws EveApiException
+     * @return array
+     */
+    public static function get(User $user, ?array $data = null): array
+    {
+        $api = self::getApiInstance('LocationApi', $user);
+
+        try {
+            $result = $api->getCharactersCharacterIdLocation(
+                $user->characterId,
+                static::$dataSource,
+                '',
+                $user->token
+            );
+        } catch (ApiException $e) {
+            $errorResponse = json_decode(
+                $e->getResponseBody(),
+                true,
+                512,
+                JSON_THROW_ON_ERROR
+            );
+
+            if ($errorResponse['error'] == 'token is expired') {
+                throw new EveApiTokenExpiredException('token is expired');
+            }
+
+            throw new EveApiException($e->getMessage(), $e->getCode());
+        }
+
+        if (!$result instanceof GetCharactersCharacterIdLocationOk) {
+            throw new EveApiException('failed to get character location');
+        }
+
+        return self::toArray($result);
+    }
+}
