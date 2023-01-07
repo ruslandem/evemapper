a29b0673d8930b33fcf676446cd6a7cb9ab3cc8b
Ruslan Demchenko <33548640+ruslandem@users.noreply.github.com>
2023-01-06 21:59:54 -0500

    Update database.php

--- config/database.php
+++ config/database.php
@@ -8,7 +8,7 @@ return [
     |--------------------------------------------------------------------------
     */
 
-    'default' => env('DB_CONNECTION', 'mysql'),
+    'default' => env('DB_CONNECTION', 'app'),
 
     /*
     |--------------------------------------------------------------------------
@@ -21,11 +21,6 @@ return [
         'app' => [
             'driver' => 'sqlite',
             'database' => database_path(env('DB_APP', '')),
-        ],
-
-        'eve' => [
-            'driver' => 'sqlite',
-            'database' => database_path(env('DB_EVE', '')),
         ],
 
     ],
