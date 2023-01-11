6d8d73da51996456100a95f433c3d3168b675fe5
Ruslan Demchenko <ruslan.demchenko@gmail.com>
2023-01-11 10:20:32 -0500

    make cachable of invType.materials

--- app/Models/InvType.php
+++ app/Models/InvType.php
@@ -2,14 +2,18 @@
 
 namespace App\Models;
 
+use App\Traits\HasModelCachableAttributes;
 use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Support\Facades\DB;
 
 class InvType extends Model
 {
-    use HasFactory;
+    use HasFactory, HasModelCachableAttributes;
+    /**
+     * Value of activityID for manufacturing activities.
+     */
+    public const MANUFACTURE_ACTIVITY = 1;
 
     protected $connection = 'app';
 
@@ -23,10 +27,17 @@ class InvType extends Model
     protected function materials(): Attribute
     {
         return Attribute::make(
-            get: fn () =>
-            DB::connection('app')->table('industryActivityMaterials')
-                ->where(['typeID' => $this->typeID, 'activityID' => 1])
-                ->get(['materialTypeID', 'quantity'])
+            get: function () {
+                return $this->useAttributeCache('materials', function () {
+                    return $this->getConnection()
+                        ->table('industryActivityMaterials')
+                        ->where([
+                            'typeID' => $this->typeID,
+                            'activityID' => self::MANUFACTURE_ACTIVITY
+                        ])
+                        ->get(['materialTypeID', 'quantity']);
+                });
+            }
         );
     }
 }
