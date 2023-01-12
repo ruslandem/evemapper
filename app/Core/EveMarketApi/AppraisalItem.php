36d32f05082e5bc1dfe7a99b0e238f9bc5e1992c
Ruslan Demchenko <ruslan.demchenko@gmail.com>
2023-01-12 13:30:04 -0500

    add AppraisalItem class

---
+++ app/Core/EveMarketApi/AppraisalItem.php
@@ -0,0 +1,134 @@
+<?php
+
+namespace App\Core\EveMarketApi;
+
+class AppraisalItem
+{
+    protected int $typeId;
+    protected int $sellVolume;
+    protected int $sellPrice;
+    protected int $buyVolume;
+    protected int $buyPrice;
+
+    function __construct(?array $array)
+    {
+        if ($array) {
+            $this->typeId = $array['typeId'];
+            $this->sellVolume = $array['sellVolume'];
+            $this->sellPrice = $array['sellPrice'];
+            $this->buyVolume = $array['buyVolume'];
+            $this->buyPrice = $array['buyPrice'];
+        }
+    }
+
+    public function toArray(): array
+    {
+        return [
+            'typeId' => $this->typeId,
+            'sellVolume' => $this->sellVolume,
+            'sellPrice' => $this->sellPrice,
+            'buyVolume' => $this->buyVolume,
+            'buyPrice' => $this->buyPrice
+        ];
+    }
+
+    /**
+     * Get the value of typeId
+     */
+    public function getTypeId()
+    {
+        return $this->typeId;
+    }
+
+    /**
+     * Set the value of typeId
+     *
+     * @return  self
+     */
+    public function setTypeId($typeId)
+    {
+        $this->typeId = $typeId;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of sellVolume
+     */
+    public function getSellVolume()
+    {
+        return $this->sellVolume;
+    }
+
+    /**
+     * Set the value of sellVolume
+     *
+     * @return  self
+     */
+    public function setSellVolume($sellVolume)
+    {
+        $this->sellVolume = $sellVolume;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of sellPrice
+     */
+    public function getSellPrice()
+    {
+        return $this->sellPrice;
+    }
+
+    /**
+     * Set the value of sellPrice
+     *
+     * @return  self
+     */
+    public function setSellPrice($sellPrice)
+    {
+        $this->sellPrice = $sellPrice;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of buyVolume
+     */
+    public function getBuyVolume()
+    {
+        return $this->buyVolume;
+    }
+
+    /**
+     * Set the value of buyVolume
+     *
+     * @return  self
+     */
+    public function setBuyVolume($buyVolume)
+    {
+        $this->buyVolume = $buyVolume;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of buyPrice
+     */
+    public function getBuyPrice()
+    {
+        return $this->buyPrice;
+    }
+
+    /**
+     * Set the value of buyPrice
+     *
+     * @return  self
+     */
+    public function setBuyPrice($buyPrice)
+    {
+        $this->buyPrice = $buyPrice;
+
+        return $this;
+    }
+}
