764562f719e42c0529003ee636cb03621410f2ce
Ruslan Demchenko <ruslan.demchenko@gmail.com>
2023-01-25 01:43:08 -0500

    add solar system and wormhole services

---
+++ app/Services/SolarSystems.php
@@ -0,0 +1,18 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Services;
+
+use App\Core\Singleton;
+use App\Models\SolarSystem;
+use App\Models\User;
+use Illuminate\Support\Collection;
+
+class SolarSystems extends Singleton
+{
+    public static function getAdjacentSystems(SolarSystem $solarSystem): Collection
+    {
+        return $solarSystem->jumps->pluck('toSolarSystem');
+    }
+}
---
+++ app/Services/Wormholes.php
@@ -0,0 +1,52 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Services;
+
+use App\Core\Singleton;
+use App\Enums\SecuritySpace;
+use App\Models\Wormhole;
+use App\Models\WormholeClass;
+use Illuminate\Support\Collection;
+
+class Wormholes extends Singleton
+{
+    /**
+     * Get wormhole statics for the specified wormhole system
+     * 
+     * @param Wormhole $wormhole
+     * @return Collection
+     */
+    public static function getWormholeStatics(Wormhole $wormhole): Collection
+    {
+        $result = new Collection();
+
+        if ($wormhole->static) {
+            foreach (explode(',', $wormhole->static) as $staticName) {
+                $result->push(
+                    WormholeClass::where(['hole' => $staticName])->first()
+                );
+            }
+        }
+
+        return $result;
+    }
+
+    /**
+     * Return simple array of wormhole classes with specified security space
+     * 
+     * @param SecuritySpace $securitySpace
+     * @return array
+     */
+    public static function getWormholeClasses(SecuritySpace $securitySpace): array
+    {
+        return array_column(
+            WormholeClass::where(['inClass' => $securitySpace])
+                ->orderBy('hole')
+                ->get()
+                ->toArray(),
+            'hole'
+        );
+    }
+}
